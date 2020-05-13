9cac5343e21160454e5da2a858f3c277239b3cd9
张启余 <paulzhangqiyu@163.com>
2020-05-13 19:57:29 +0800

    新潮提交1

---
+++ xincao/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="wrap">
+        <h1>新潮工作内容</h1>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="stylus">
+    .wrap {
+        width 100px
+    }
+</style>
